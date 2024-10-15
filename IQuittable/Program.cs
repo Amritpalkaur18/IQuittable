73397d1b0b184952ee6409688e35493c9112a2cc
Amritpalkaur18 <palamrit0218@gmail.com>
2024-10-15 19:37:01 -0400

    Add project files.

---
+++ IQuittable/Program.cs
@@ -0,0 +1,38 @@
+﻿
+public interface IQuittable
+{
+    
+    void Quit();
+}
+
+public class Employee : IQuittable
+{
+   
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+   
+    public void Quit()
+    {
+        Console.WriteLine($"{FirstName} {LastName} has quit the job.");
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        Employee employee = new Employee() { FirstName = "John", LastName = "Doe" };
+
+       
+        IQuittable quittableEmployee = employee;
+
+       
+        quittableEmployee.Quit();
+
+       
+        Console.ReadLine();
+    }
+}
